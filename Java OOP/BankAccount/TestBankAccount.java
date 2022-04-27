54432073947dd315bba6cf715cb02ec64210b211
Fadi Abuaita <fadimabuaita@gmail.com>
2022-04-27 22:09:09 +0300

    Bank Account Assignment

---
+++ Java OOP/BankAccount/TestBankAccount.java
@@ -0,0 +1,10 @@
+public class TestBankAccount {
+    public static void main(String[] args){
+
+        BankAccount myAccount=new BankAccount(800,1000);
+        myAccount.depositMoney(200, 0);
+        myAccount.withdrawMoney(100);
+        myAccount.getAccountBalance();
+
+    }
+}
