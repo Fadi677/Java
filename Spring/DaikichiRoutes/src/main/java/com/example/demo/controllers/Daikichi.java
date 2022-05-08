90adadf44c31c6c14336ca29fdba04565473eb8c
Fadi Abuaita <fadimabuaita@gmail.com>
2022-05-08 15:32:21 +0300

    uploading assignments

--- Spring/DaikichiRoutes/src/main/java/com/example/demo/controllers/Daikichi.java
+++ Spring/DaikichiRoutes/src/main/java/com/example/demo/controllers/Daikichi.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -10,14 +11,34 @@ public class Daikichi {
     public String hello() {
             return "Welcome!";
 }
+	
 	@RequestMapping("/today")
     public String today() {
             return "Today you will find luck in all your endaovers!";
 	
 }
+	
 	@RequestMapping("/tomorrow")
     public String tomorrow() {
             return "Tomorrow,an idea will rise, so be sure to be open to new ideas!";
 	
 }
+	@RequestMapping("/travel/{city}")
+		public String travel(@PathVariable("city") String city) {
+			return "Congratulations! You will soon travel to "+ city;
+			
+		}
+
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") int number) {
+		if(number %2==1) {
+			return  "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+		else {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		
+	}
+	
+	
 }
