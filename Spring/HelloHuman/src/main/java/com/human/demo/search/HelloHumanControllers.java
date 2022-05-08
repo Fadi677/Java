d1b96165c6b498f62d7a650d15f7c95678654f01
Fadi Abuaita <fadimabuaita@gmail.com>
2022-05-08 15:57:10 +0300

    hello human assignment

--- Spring/HelloHuman/src/main/java/com/human/demo/search/HelloHumanControllers.java
+++ Spring/HelloHuman/src/main/java/com/human/demo/search/HelloHumanControllers.java
@@ -9,12 +9,25 @@ public class HelloHumanControllers {
 		
 	
 	@RequestMapping("/")
-	public String index(@RequestParam(value="name", required=false) String username) {
+	public String index(@RequestParam(value="name", required=false) String username, @RequestParam(value="last_name",required=false) String last_name
+			) {
 		if(username==null) {
 		return "Hello Human";
 	}
-		else {
+		else if(last_name==null){
 			return "Hello "+username;
 		}
-}
+		
+		else {
+			return "Hello "+username+" "+last_name;
+		}
+	}
+	
+	@RequestMapping("/search")
+	public String search(@RequestParam(value="name", required=false) String username,@RequestParam(value="number", required=false) int number) {
+		String statement="Hello "+username+" ";
+		return statement.repeat(number);
+	}
+	
+	
 }
